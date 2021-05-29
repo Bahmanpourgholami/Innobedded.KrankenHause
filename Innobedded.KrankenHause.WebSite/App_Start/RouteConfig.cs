cb995f41135a1bde1f1a24ce50251853631e43ff
Bahman <a@b.com>
2021-05-29 14:15:47 +0430

    Make Fach and Doctor Repository and implement Alles inherients between GenericRepository

--- Innobedded.KrankenHause.WebSite/App_Start/RouteConfig.cs
+++ Innobedded.KrankenHause.WebSite/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Innobedded.KrankenHause.WebSite
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Innobedded.KrankenHause.WebSite/Areas/Admin/Controllers/FachesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Innobedded.KrankenHause.Data.Context;
+using Innobedded.KrankenHause.Data.Entities;
+using Innobedded.KrankenHause.Domain.UnitOfWork;
+
+namespace Innobedded.KrankenHause.WebSite.Areas.Admin.Controllers
+{
+    public class FachesController : Controller
+    {
+        //private KrankenHausDbContext db = new KrankenHausDbContext();
+        private UnitOfWork _unitofwork;
+        public FachesController()
+        {
+            _unitofwork = new UnitOfWork();
+        }
+
+        // GET: Admin/Faches
+        public ActionResult Index()
+        {
+            //return View(db.Faches.ToList());
+            return View(_unitofwork.FachRepository.GetAll());
+        }
+
+        // GET: Admin/Faches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Fach fach = db.Faches.Find(id);
+            Fach fach = _unitofwork.FachRepository.GetById(id);
+            if (fach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fach);
+        }
+
+        // GET: Admin/Faches/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Faches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FachID,Title")] Fach fach)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Faches.Add(fach);
+                //db.SaveChanges();
+                _unitofwork.FachRepository.Insert(fach);
+                _unitofwork.FachRepository.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(fach);
+        }
+
+        // GET: Admin/Faches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Fach fach = db.Faches.Find(id);
+            Fach fach = _unitofwork.FachRepository.GetById(id);
+            if (fach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fach);
+        }
+
+        // POST: Admin/Faches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FachID,Title")] Fach fach)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(fach).State = EntityState.Modified;
+                //db.SaveChanges();
+                _unitofwork.FachRepository.Update(fach);
+                _unitofwork.FachRepository.Save();
+                return RedirectToAction("Index");
+            }
+            return View(fach);
+        }
+
+        // GET: Admin/Faches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fach fach = _unitofwork.FachRepository.GetById(id);
+            if (fach == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fach);
+        }
+
+        // POST: Admin/Faches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Fach fach = _unitofwork.FachRepository.GetById(id);
+            _unitofwork.FachRepository.Delete(fach);
+            _unitofwork.FachRepository.Save();
+            
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SpecialFach(string fach)
+        {
+            fach = "کیر";
+            var Temp = _unitofwork.FachRepository.GetSpecialFach(fach);
+            if (Temp == null)
+            {
+                HttpNotFound();
+            }
+            else
+            return View(Temp);
+
+            return null;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _unitofwork.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
