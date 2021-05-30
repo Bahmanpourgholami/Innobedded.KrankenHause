9e89d975ffae79877c36652a0dd1a9172eace7f0
Bahman <a@b.com>
2021-05-30 15:16:30 +0430

    Add Email Field To UserTable and migration
    Email can to used for Return Forgetet Password in Identity2 System

---
+++ Innobedded.KrankenHause.Data/Migrations/202105301043537_add_EmailField_to_UserTable.cs
@@ -0,0 +1,18 @@
+﻿namespace Innobedded.KrankenHause.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_EmailField_to_UserTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "Email", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "Email");
+        }
+    }
+}
