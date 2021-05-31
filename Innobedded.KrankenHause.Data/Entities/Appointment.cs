334f8fc249e8b67b5de1ad12168d9df9a27416b2
Bahman <a@b.com>
2021-05-31 15:41:24 +0430

    Reserve Table for Appointment created

---
+++ Innobedded.KrankenHause.Data/Entities/Appointment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innobedded.KrankenHause.Data.Entities
+{
+    public class Appointment
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Fullname")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Reserve Date")]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime ReserveDate { get; set; }
+
+        [Display(Name = "Telefon")]
+        public string Telefon { get; set; }
+
+        [Display(Name = "Message")]
+        [MaxLength(350)]
+        public string Description { get; set; }
+    }
+}
---
+++ Innobedded.KrankenHause.Data/Migrations/202105311110278_Reserve_Table_create.cs
@@ -0,0 +1,16 @@
+﻿namespace Innobedded.KrankenHause.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Reserve_Table_create : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
